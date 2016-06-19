4c9458c0076257e563c9c81006c0dee8534047ed
Sergei Mnishko <e1525104@student.tuwien.ac.at>
2016-06-19 18:00:00 +0200

    run without errors

--- src/main/camel/routes/Accept30PercentRoute.java
+++ src/main/camel/routes/Accept30PercentRoute.java
@@ -1,10 +1,5 @@
 package main.camel.routes;
 
-import main.camel.beans.Accept30PercentBean;
-import main.camel.beans.MakeConfirmationBean;
-import main.model.CarOrder;
-import main.model.Customer;
-import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -21,37 +16,37 @@ public class Accept30PercentRoute extends RouteBuilder {
 //                .to("direct:queryStock");
 
         from("direct:accept30percent")
-                .routeId("Accept30percentRoute-Entrance")
-                .to("jms:queue:Accept30percentRouteDispatch?messageConverter=#myMessageConverter");
-
-        from("jms:queue:Accept30percentRouteDispatch").delay(5000)
-                .routeId("Accept30percentRouteDispatch")
-                .bean(Accept30PercentBean.class)
-                .choice()
-                .when(header("is30percentPaid").isEqualTo(false))
-                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t\t|\t Order Nr.: ${header.orderID} \t|\t From Accept30percentRouteDispatch to Accept30percentRouteDispatch - LOOP \t|\t ${body}")
-                .to("jms:queue:Accept30percentRouteDispatch")
-                .otherwise()
-                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t\t|\t Order Nr.: ${header.orderID} \t|\t From Accept30percentRouteDispatch to InformCustomerAndAccept30Percent \t|\t ${body}")
-                .to("jms:direct:informCustomerAndAccept30Percent")
-                .endChoice();
-
-        from("jms:direct:informCustomerAndAccept30Percent")
-                .routeId("Accept30percentRoute-Leaving")
-                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t|\t Order Nr.: ${header.orderID} \t|\t From InformCustomerAndAccept30Percent to InformCustomer & QueryStock \t|\t ${body}")
-                .multicast()
-                .to("jms:direct:makeConfirmation")
+//                .routeId("Accept30percentRoute-Entrance")
+//                .to("jms:queue:Accept30percentRouteDispatch?messageConverter=#myMessageConverter");
+//
+//        from("jms:queue:Accept30percentRouteDispatch").delay(5000)
+//                .routeId("Accept30percentRouteDispatch")
+//                .bean(Accept30PercentBean.class)
+//                .choice()
+//                .when(header("is30percentPaid").isEqualTo(false))
+//                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t\t|\t Order Nr.: ${header.orderID} \t|\t From Accept30percentRouteDispatch to Accept30percentRouteDispatch - LOOP \t|\t ${body}")
+//                .to("jms:queue:Accept30percentRouteDispatch")
+//                .otherwise()
+//                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t\t|\t Order Nr.: ${header.orderID} \t|\t From Accept30percentRouteDispatch to InformCustomerAndAccept30Percent \t|\t ${body}")
+//                .to("jms:direct:informCustomerAndAccept30Percent")
+//                .endChoice();
+//
+//        from("jms:direct:informCustomerAndAccept30Percent")
+//                .routeId("Accept30percentRoute-Leaving")
+//                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t|\t Order Nr.: ${header.orderID} \t|\t From InformCustomerAndAccept30Percent to InformCustomer & QueryStock \t|\t ${body}")
+//                .multicast()
+//                .to("jms:direct:makeConfirmation")
                 .to("direct:queryStock");
 
         // @andrey TODO remove jpa call, and just take the customer from the body
-        from("jms:direct:makeConfirmation")
-            .routeId("MakeConfirmationRouter")
-                    .pollEnrich("jpa:Customer" +
-                            "?consumer.query=select c from Customer c where c.id=1&consumeDelete=false")
-            .bean(MakeConfirmationBean.class, "makeConfirmation")
-                    .multicast()
-                    .to("file:backup/30percent/?fileName=${date:now:yyyyMMdd}-${in.header.orderID}.txt&autoCreate=true")
-                    //.to("file://C:\\Users\\maland\\AppData\\Roaming\\SmartCompany\\30PercentConfirmation")
-                    .to("seda:informCustomer"); // TODO route to clever
+//        from("jms:direct:makeConfirmation")
+//            .routeId("MakeConfirmationRouter")
+//                    .pollEnrich("jpa:Customer" +
+//                            "?consumer.query=select c from Customer c where c.id=1&consumeDelete=false")
+//            .bean(MakeConfirmationBean.class, "makeConfirmation")
+//                    .multicast()
+//                    .to("file:backup/30percent/?fileName=${date:now:yyyyMMdd}-${in.header.orderID}.txt&autoCreate=true")
+//                    //.to("file://C:\\Users\\maland\\AppData\\Roaming\\SmartCompany\\30PercentConfirmation")
+//                    .to("seda:informCustomer"); // TODO route to clever
     }
 }
--- src/main/camel/routes/Accept70PercentRoute.java
+++ src/main/camel/routes/Accept70PercentRoute.java
@@ -1,7 +1,5 @@
 package main.camel.routes;
 
-import main.camel.beans.Accept70PercentBean;
-import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -18,24 +16,24 @@ public class Accept70PercentRoute extends RouteBuilder {
 
 
         from("direct:accept70percent")
-                .routeId("Accept70percentRoute-Entrance")
-                .to("jms:queue:Accept70percentRouteDispatch");
-
-        from("jms:queue:Accept70percentRouteDispatch").delay(5000)
-                .routeId("Accept70percentRouteDispatch")
-                .bean(Accept70PercentBean.class)
-                .choice()
-                .when(header("is70percentPaid").isEqualTo(false))
-                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t\t|\t Order Nr.: ${header.orderID} \t|\t From Accept70percentRouteDispatch to Accept70percentRouteDispatch - LOOP \t|\t ${body}")
-                .to("jms:queue:Accept70percentRouteDispatch")
-                .otherwise()
-                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t\t|\t Order Nr.: ${header.orderID} \t|\t From Accept70percentRouteDispatch to InformCustomerAndAccept70Percent \t|\t ${body}")
-                .to("jms:direct:informCustomerAndAccept70Percent")
-                .endChoice();
-
-        from("jms:direct:informCustomerAndAccept70Percent")
-                .routeId("Accept70percentRoute-Leaving")
-                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t|\t Order Nr.: ${header.orderID} \t|\t From InformCustomerAndAccept70Percent to InformCustomer & QueryStock \t|\t ${body}")
+//                .routeId("Accept70percentRoute-Entrance")
+//                .to("jms:queue:Accept70percentRouteDispatch");
+//
+//        from("jms:queue:Accept70percentRouteDispatch").delay(5000)
+//                .routeId("Accept70percentRouteDispatch")
+//                .bean(Accept70PercentBean.class)
+//                .choice()
+//                .when(header("is70percentPaid").isEqualTo(false))
+//                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t\t|\t Order Nr.: ${header.orderID} \t|\t From Accept70percentRouteDispatch to Accept70percentRouteDispatch - LOOP \t|\t ${body}")
+//                .to("jms:queue:Accept70percentRouteDispatch")
+//                .otherwise()
+//                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t\t|\t Order Nr.: ${header.orderID} \t|\t From Accept70percentRouteDispatch to InformCustomerAndAccept70Percent \t|\t ${body}")
+//                .to("jms:direct:informCustomerAndAccept70Percent")
+//                .endChoice();
+//
+//        from("jms:direct:informCustomerAndAccept70Percent")
+//                .routeId("Accept70percentRoute-Leaving")
+//                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t|\t Order Nr.: ${header.orderID} \t|\t From InformCustomerAndAccept70Percent to InformCustomer & QueryStock \t|\t ${body}")
                 .multicast()
                 .to("direct:handOverOrder")
                 .to("seda:informCustomer");
