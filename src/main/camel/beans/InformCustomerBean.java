60d96546cb58e04485caa46c3f491372b867f41b
Andreas Kocman <e0302840@student.tuwien.ac.at>
2016-06-27 20:39:20 +0200

    #33 InformCustomer updated to represent states more clearly

---
+++ src/main/camel/beans/InformCustomerBean.java
@@ -0,0 +1,64 @@
+package main.camel.beans;
+
+import main.model.CarOrder;
+import main.model.enums.OrderStatus;
+import org.apache.camel.Exchange;
+import org.apache.camel.Handler;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+import static main.model.enums.OrderStatus.NEW;
+
+@Component
+public class InformCustomerBean {
+
+    private static final Logger LOGGER = Logger.getLogger("FILE");
+
+    @Handler
+    public void process(Exchange exchange) throws Exception {
+        CarOrder carOrder = exchange.getIn().getBody(CarOrder.class);
+        exchange.getIn().setHeader("to", carOrder.getCustomerFK().getEmail());
+        exchange.getIn().setHeader("status", carOrder.getStatus());
+
+        String status = "";
+
+        switch(carOrder.getStatus()){
+            case NEW:
+                status = "your order has been received.";
+                break;
+            case INITIALPAYMENTACCEPTED:
+                status = "your initial payment has been transferred.";
+                break;
+            case CREDITCHECK:
+                if (exchange.getIn().getHeader("creditCheck")==null) {
+                    status = "your credit solvency is currently checked by our partners.";
+                }else {
+                    status = "your credit check yielded the result " +
+                            (exchange.getIn().getHeader("creditCheck") == "true" ? "passed" : "failed");
+                }
+                break;
+            case CREDITDENIED:
+                status = "your order has been received. Unfortunately your credit was denied. Please contact us.";
+                break;
+            case ASSEMBLING:
+                status = "your car is currently being assembled.";
+                break;
+            case ASSEMBLINGFINISHED:
+                status = "your car has finished the assembling process.";
+                break;
+            case PAYMENTCOMPLETED:
+                status = "your payment has been received in full.";
+                break;
+            case DELIVERED:
+                status = "your car is on its way to you. Enjoy and drive safely!";
+        }
+
+        exchange.getIn().setBody("Dear " + carOrder.getCustomerFK().getFirstName() + ",\n" +
+                "We are pleased to inform you that  " + status +
+                "\n\n" +
+                "Kind regards,\n" +
+                "your smart car company bot");
+    }
+}
--- src/main/camel/routes/InformCustomerRoute.java
+++ src/main/camel/routes/InformCustomerRoute.java
@@ -1,5 +1,6 @@
 package main.camel.routes;
 
+import main.camel.beans.InformCustomerBean;
 import main.model.CarOrder;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
@@ -10,37 +11,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class InformCustomerRoute extends RouteBuilder {
 
-    // mail component doc: http://camel.apache.org/mail.html
 
     @Override
     public void configure() throws Exception
     {
-        // the mail component takes the body and sends it
-
-        //String recipient = "mathck@gmail.com"; // for multiple may be seperated by ","
 
         //--------------------------------------------------
         // ROUTE
         //--------------------------------------------------
         from("direct:informCustomer")
                 .routeId("InformCustomerRoute")
-                //.setBody(simple(getEmailBody("name", "status")))// TODO insert customerName and orderStatus
-                //.log(LoggingLevel.INFO, "FILE", "${routeId}\t\t\t\t|\t OrderID.: ${header.orderID} 	|	Sending mail - Body is: ${body}")
                 .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t\t\t|\t OrderID.: ${header.orderID} \t|\t Sending mail to ${body.getCustomerFK.getEmail} for status: ${body.getStatus}")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        CarOrder carOrder = exchange.getIn().getBody(CarOrder.class);
-                        exchange.getIn().setHeader("to", carOrder.getCustomerFK().getEmail());
-                        exchange.getIn().setHeader("status", carOrder.getStatus());
-                        exchange.getIn().setBody("Dear " + carOrder.getCustomerFK().getFirstName() + ",\n" +
-                                "We are pleased to inform you that your order status is " + carOrder.getStatus() +
-                                "\n\n" +
-                                "Kind regards,\n" +
-                                "your smart car company bot");
-
-                    }
-                })
+                .bean(InformCustomerBean.class)
                 .to("smtp://smtp.gmail.com" +
                         "?port={{mail.port}}" +
                         "&username={{mail.username}}" +
@@ -52,44 +34,5 @@ public class InformCustomerRoute extends RouteBuilder {
                 .log(LoggingLevel.INFO,"FILE", "${routeId} \t\t\t\t|\t OrderID.: ${header.orderID} \t|\t Saving Mail for Order ${header.orderID} and Status ${header.status} as ${date:now:yyyyMMdd}-${header.orderID}-${header.status}.txt")
                 .to("file:backup/orders/mails/?fileName=${date:now:yyyyMMdd}-${header.orderID}-${header.status}.txt&autoCreate=true");;
 
-        /*
-        from("seda:informCustomerClever") TODO Andr
-                .routeId("InformCustomerRouteClever")
-                .log(LoggingLevel.INFO, "FILE", "${routeId} \t\t\t\t|\t OrderID.: ${header.orderID} \t|\t ")
-          //      .setBody(simple(getEmailBody("name", "status")))// TODO insert customerName and orderStatus
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-
-                        Customer customer = exchange.getIn().getBody(Customer.class);
-
-                        String path = new String();
-                        path = "C:\\Users\\maland\\AppData\\Roaming\\SmartCompany\\30PercentConfirmation" + "\\"+ exchange.getIn().getHeader("fileName").toString();
-
-                        exchange.getIn().addAttachment(exchange.getIn().getHeader("fileName").toString(), new DataHandler(new FileDataSource(path)));
-                    }
-                })
-                .to("smtp://smtp.gmail.com" +
-                        "?port={{mail.port}}" +
-                        "&username={{mail.username}}" +
-                        "&password={{mail.password}}" +
-                        "&subject=Your Order" +
-                        "&mail.smtp.auth={{mail.smtp.auth}}" +
-                        "&mail.smtp.starttls.enable={{mail.smtp.starttls.enable}}" +
-                        "&to=" + recipient);
-*/
     }
-
-/*    private String getEmailBody(Exchange exchange) {
-        CarOrder order = ((CarOrder) exchange.getIn().getBody());
-        exchange.getOut().setBody(exchange.getIn().getBody());
-        exchange.getOut().setHeaders(exchange.getIn().getHeaders());
-        exchange.getOut().setHeader("recipient", order.getCustomerFK().getEmail());
-        return "Dear " + order.getCustomerFK().getFirstName() + ",\n" +
-                "We are pleased to inform you that your order status is " + order.getStatus() +
-                "\n\n" +
-                "Kind regards,\n" +
-                "your smart car company bot";
-
-    }*/
 }
